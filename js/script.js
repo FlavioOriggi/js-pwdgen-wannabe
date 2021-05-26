f7f752c8a89e62006ad45057579d384a3de676cd
Flavio <flavio.origgi@outlook.it>
2021-05-26 14:29:26 +0200

    funziona

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+var nome = prompt("Digita il tuo nome");
+
+var cognome = prompt("Digita il tuo cognome");
+
+var colore = prompt("Digita il tuo colore preferito");
+
+var risultato = nome+cognome+colore+21;
+
+document.write(risultato)
+
+
